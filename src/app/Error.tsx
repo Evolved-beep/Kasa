95c8efc73d64b4ee8b414428358994af744b6a77
Evolved-beep <maxime.magnier@outlook.com>
2024-02-23 13:04:45 +0100

    Error page fixed

--- src/app/Error.tsx
+++ src/app/Error.tsx
@@ -5,11 +5,10 @@ const Error = () => {
     return(
         <div className="h-screen flex justify-center items-center flex-col text-[#FF6666] text-center">
             <h1 className="text-6xl mb-12 font-extrabold">404</h1>
-            <h2 className="text-3xl mb-8">Oups la page que vous demandez n'existe pas !</h2>
-            <Link href={"/"} className="underline p-2">
+            <h2 className="text-3xl mb-8">Oups la page que vous demandez n&apos;existe pas</h2>
+            <Link href={`/`} className="underline p-2">
                 <p className="text-xl">Retournez à la page principal</p>
             </Link>
-
         </div>
     )
 }
